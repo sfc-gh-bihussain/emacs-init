bea09a86553b93214ba2c82acb000574e8452282
Bilaal Hussain <bilaalhussain@gmail.com>
2019-04-21 13:28:57 -0400

    Add working swiper

--- init.el
+++ init.el
@@ -57,6 +57,25 @@
        ((t (:inherit ace-jump-face-foreground :height 3.0)))))
     ))
 
+;;Better searching
+(use-package swiper
+  :ensure t
+  :config
+  (progn
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (setq enable-recursive-minibuffers t)
+    ;; enable this if you want `swiper' to use it
+    ;; (setq search-default-mode #'char-fold-to-regexp)
+    (global-set-key "\C-s" 'swiper)
+    (global-set-key (kbd "M-x") 'counsel-M-x)
+    (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+    (global-set-key (kbd "C-c g") 'counsel-git)
+    (global-set-key (kbd "C-c j") 'counsel-git-grep)
+    (global-set-key (kbd "C-x l") 'counsel-locate)
+    (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+    ))
+
 
 
 ;;Commands for debugging missing colors in xterm
