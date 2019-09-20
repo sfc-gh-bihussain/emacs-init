2e1387f07eac87ffebdacd1743b4840c2b467413
unknown <bilaalhussain1@gmail.com>
2019-09-19 21:27:07 -0400

    Rainbow delimeters, dimmer, golden ratio mode, linum only on programming mode

--- ui.el
+++ ui.el
@@ -1,4 +1,5 @@
-(global-linum-mode 1)
+(add-hook 'text-mode-hook 'linum-mode)
+(add-hook 'prog-mode-hook 'linum-mode)
 
 (tool-bar-mode -1)
 
@@ -33,4 +34,27 @@
   :ensure t
   :config
   (golden-ratio-mode 1))
+
+
+(use-package dimmer
+  :ensure t
+  :config
+  (dimmer-mode 1))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :config
+  (rainbow-delimiters-mode 1)
+  )
+(setq rainbow-delimiters-depth-1-face "red")
+
+;; (use-package dired-sidebar
+;;   :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+;;   :ensure t
+;;   :commands (dired-sidebar-toggle-sidebar)
+;;   :config
+;;   (setq dired-sidebar-subtree-line-prefix "__")
+;;   (setq dired-sidebar-theme 'vscode)
+;;   (setq dired-sidebar-use-term-integration t)
+;;   (setq dired-sidebar-use-custom-font t))
 
