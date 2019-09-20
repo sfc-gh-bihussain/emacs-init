e519c4b4212d72e071cc507fc1d88640c0eaac59
unknown <bilaalhussain1@gmail.com>
2019-09-19 21:28:15 -0400

    Updated registers

--- registers.el
+++ registers.el
@@ -1,12 +1,10 @@
-;;TODO: Make these bindings platform agnostic
-;;--Import register bindings from file
-;;--Add platform specific home path
-
 (if (string= (system-name) "DESKTOP-24FKRDK")
     (progn
       (set-register ?i '(file . "C:/Users/User/AppData/Roaming/.emacs.d/emacs-init/emacs-init.el"))
       (set-register ?t '(file . "C:/Users/User/Documents/Tobyx"))
       (set-register ?d '(file . "C:/Users/User/Drive"))
+      (set-register ?4 '(file . "C:/Users/User/Drive/CS341"))
+      (set-register ?c '(file . "C:/Users/User/Documents"))
       (set-register ?o '(file . "C:/Users/User/Downloads"))
       (set-register ?m '(file . "C:/MinGW/bin")))
   )
